aefb5f871cf1dbe7bbcbf4ed040c9736c4dd6631
bigari <bigari7@gmail.com>
2019-11-22 14:46:05 +0100

    Add link to signup in signin

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,3 +10,9 @@ export default {
   })
 };
 </script>
+
+<style>
+.no-deco {
+  text-decoration: none;
+}
+</style>
--- frontend/src/views/Signin.vue
+++ frontend/src/views/Signin.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="signin">
-    <v-app id="inspire">
+    <v-app>
       <v-content>
         <v-container class="fill-height" fluid>
           <v-row align="center" justify="center">
@@ -33,6 +33,12 @@
                   <v-btn color="primary">Signin</v-btn>
                 </v-card-actions>
               </v-card>
+              <div class="d-flex mt-4">
+                <p class="mx-auto title font-weight-regular mt-2">
+                  Don't have an account yet?
+                  <router-link to="/signup" class="no-deco">Signup</router-link>
+                </p>
+              </div>
             </v-col>
           </v-row>
         </v-container>
