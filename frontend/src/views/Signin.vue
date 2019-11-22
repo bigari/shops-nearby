a7396e1b006eb5fe3a8f401323d58458ecb010e2
bigari <bigari7@gmail.com>
2019-11-22 14:21:39 +0100

    Add signin ui

--- frontend/src/views/Signin.vue
+++ frontend/src/views/Signin.vue
@@ -1,5 +1,44 @@
 <template>
-  <div class="signin">Signin</div>
+  <div class="signin">
+    <v-app id="inspire">
+      <v-content>
+        <v-container class="fill-height" fluid>
+          <v-row align="center" justify="center">
+            <v-col cols="12" sm="8" md="4">
+              <v-card class="elevation-3">
+                <v-toolbar color="primary" dark flat>
+                  <v-toolbar-title class="headline"
+                    >Signin Form</v-toolbar-title
+                  >
+                </v-toolbar>
+                <v-card-text>
+                  <v-form>
+                    <v-text-field
+                      label="Email"
+                      name="email"
+                      prepend-icon="person"
+                      type="text"
+                    />
+                    <v-text-field
+                      id="password"
+                      label="Password"
+                      name="password"
+                      prepend-icon="lock"
+                      type="password"
+                    />
+                  </v-form>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer />
+                  <v-btn color="primary">Signin</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-content>
+    </v-app>
+  </div>
 </template>
 
 <script>
