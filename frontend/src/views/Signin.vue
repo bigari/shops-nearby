a79a86907426653dc6d8bab5c558444ffb8541fb
bigari <bigari7@gmail.com>
2019-11-23 22:10:40 +0100

    Refactor Signin and Signup views
    Create a component that can center its children vertically and be used by both Signin and Signup views

--- frontend/src/views/Signin.vue
+++ frontend/src/views/Signin.vue
@@ -1,74 +1,65 @@
 <template>
   <div class="signin">
-    <v-app>
-      <v-content>
-        <v-container class="fill-height" fluid>
-          <v-row align="center" justify="center">
-            <v-col cols="12" sm="8" md="4">
-              <v-alert
-                v-if="error"
-                class="mb-4"
-                border="right"
-                colored-border
-                type="error"
-                elevation="2"
-                >{{ error }}</v-alert
-              >
-              <v-card class="elevation-3">
-                <v-toolbar color="primary" dark flat>
-                  <v-toolbar-title class="headline"
-                    >Signin Form</v-toolbar-title
-                  >
-                </v-toolbar>
-                <v-card-text>
-                  <v-form v-model="isValid">
-                    <v-text-field
-                      v-model="email"
-                      label="Email"
-                      :rules="[rules.required]"
-                      name="email"
-                      prepend-icon="person"
-                      type="text"
-                    />
-                    <v-text-field
-                      v-model="password"
-                      :rules="[rules.required]"
-                      label="Password"
-                      name="password"
-                      prepend-icon="lock"
-                      type="password"
-                    />
-                  </v-form>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer />
-                  <v-btn
-                    color="primary"
-                    :disabled="!isValid"
-                    @click.native="signin"
-                    >Signin</v-btn
-                  >
-                </v-card-actions>
-              </v-card>
-              <div class="d-flex mt-4">
-                <p class="mx-auto title font-weight-regular mt-2">
-                  Don't have an account yet?
-                  <router-link to="/signup" class="no-deco">Signup</router-link>
-                </p>
-              </div>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-content>
-    </v-app>
+    <vertical-center>
+      <v-alert
+        v-if="error"
+        class="mb-4"
+        border="right"
+        colored-border
+        type="error"
+        elevation="2"
+        >{{ error }}</v-alert
+      >
+      <v-card class="elevation-3">
+        <v-toolbar color="primary" dark flat>
+          <v-toolbar-title class="headline">Signin Form</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+          <v-form v-model="isValid">
+            <v-text-field
+              v-model="email"
+              label="Email"
+              :rules="[rules.required]"
+              name="email"
+              prepend-icon="person"
+              type="text"
+            />
+            <v-text-field
+              v-model="password"
+              :rules="[rules.required]"
+              label="Password"
+              name="password"
+              prepend-icon="lock"
+              type="password"
+            />
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" :disabled="!isValid" @click.native="signin"
+            >Signin</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+      <div class="d-flex mt-4">
+        <p class="mx-auto title font-weight-regular mt-2">
+          Don't have an account yet?
+          <router-link to="/signup" class="no-deco">Signup</router-link>
+        </p>
+      </div>
+    </vertical-center>
   </div>
 </template>
 
 <script>
 import { rules } from "@/utils/rules";
+import VerticalCenter from "@/components/VerticalCenter";
 
 export default {
   name: "signin",
+  components: {
+    VerticalCenter
+  },
   data() {
     return {
       error: "",
--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -1,81 +1,72 @@
 <template>
   <div class="signup">
-    <v-app>
-      <v-content>
-        <v-container class="fill-height" fluid>
-          <v-row align="center" justify="center">
-            <v-col cols="12" sm="8" md="4">
-              <v-alert
-                v-if="error"
-                class="mb-4"
-                border="right"
-                colored-border
-                type="error"
-                elevation="2"
-                >{{ error }}</v-alert
-              >
-              <v-card class="elevation-3">
-                <v-toolbar color="primary" dark flat>
-                  <v-toolbar-title class="headline"
-                    >Signup Form</v-toolbar-title
-                  >
-                </v-toolbar>
-                <v-card-text>
-                  <v-form v-model="isValid">
-                    <v-text-field
-                      label="Email"
-                      v-model="email"
-                      name="email"
-                      :rules="[rules.required, rules.emailFormat]"
-                      prepend-icon="person"
-                      type="text"
-                    />
-                    <v-text-field
-                      label="Password"
-                      v-model="password"
-                      name="password"
-                      :rules="[rules.required, rules.min]"
-                      prepend-icon="lock"
-                      type="password"
-                    />
-                    <v-text-field
-                      label="Confirm Password"
-                      name="confirm-password"
-                      :rules="[passwordMatchingRule]"
-                      prepend-icon="lock"
-                      type="password"
-                    />
-                  </v-form>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer />
-                  <v-btn
-                    color="primary"
-                    :disabled="!isValid"
-                    @click.native="signup"
-                    >Signup</v-btn
-                  >
-                </v-card-actions>
-              </v-card>
-              <div class="d-flex mt-4">
-                <p class="mx-auto title font-weight-regular mt-2">
-                  Already have an account?
-                  <router-link to="/signin" class="no-deco">Signin</router-link>
-                </p>
-              </div>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-content>
-    </v-app>
+    <vertical-center>
+      <v-alert
+        v-if="error"
+        class="mb-4"
+        border="right"
+        colored-border
+        type="error"
+        elevation="2"
+        >{{ error }}</v-alert
+      >
+      <v-card class="elevation-3">
+        <v-toolbar color="primary" dark flat>
+          <v-toolbar-title class="headline">Signup Form</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+          <v-form v-model="isValid">
+            <v-text-field
+              label="Email"
+              v-model="email"
+              name="email"
+              :rules="[rules.required, rules.emailFormat]"
+              prepend-icon="person"
+              type="text"
+            />
+            <v-text-field
+              label="Password"
+              v-model="password"
+              name="password"
+              :rules="[rules.required, rules.min]"
+              prepend-icon="lock"
+              type="password"
+            />
+            <v-text-field
+              label="Confirm Password"
+              name="confirm-password"
+              :rules="[passwordMatchingRule]"
+              prepend-icon="lock"
+              type="password"
+            />
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" :disabled="!isValid" @click.native="signup"
+            >Signup</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+      <div class="d-flex mt-4">
+        <p class="mx-auto title font-weight-regular mt-2">
+          Already have an account?
+          <router-link to="/signin" class="no-deco">Signin</router-link>
+        </p>
+      </div>
+    </vertical-center>
   </div>
 </template>
 
 <script>
 import { rules } from "@/utils/rules";
+import VerticalCenter from "@/components/VerticalCenter";
 
 export default {
   name: "signup",
+  components: {
+    VerticalCenter
+  },
   data() {
     return {
       error: "",
