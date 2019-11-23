d8f91cef6667347b3961a82d6f6c2f4f49609e85
bigari <bigari7@gmail.com>
2019-11-23 18:33:41 +0100

    Implement actions to fetch shops and categories

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -6,11 +6,47 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    user: null
+    shops: [],
+    categories: {},
+    user: null,
+    config: {
+      headers: { ...API.HEADERS }
+    }
   },
   mutations: {
     SET_USER: (state, user) => {
       state.user = user;
+    },
+    /**
+     * Set Authorization header
+     * @param  {State} state Vuex state
+     * @param  {Array} token Authentication token
+     */
+    ADD_AUTH: (state, token) => {
+      state.config.headers.Authorization = `Bearer ${token}`;
+    },
+
+    /**
+     * Populate categories
+     * @param  {State} state Vuex state
+     * @param  {Array} categories List of categories
+     */
+    SET_CATEGORIES: (state, categories) => {
+      categories.forEach(
+        category => (state.categories[category.id] = category.name)
+      );
+    },
+    /**
+     * Map each shop to its category and set state.shops
+     * @param  {State} state Vuex state
+     * @param  {Array} shops List of shops
+     */
+    SET_SHOPS: (state, shops) => {
+      const categorizedShops = [...shops].map(shop => {
+        shop.category = state.categories[shop.categoryId];
+        return shop;
+      });
+      state.shops = categorizedShops;
     }
   },
   actions: {
@@ -33,7 +69,7 @@ export default new Vuex.Store({
      * @return {ApiErrorResponse}
      */
     SIGNIN: async (context, credentials) => {
-      let response = await fetch(`${API.ENDPOINT}/users/login`, {
+      const response = await fetch(`${API.ENDPOINT}/users/login`, {
         method: "POST",
         headers: API.HEADERS,
         body: JSON.stringify({
@@ -45,12 +81,12 @@ export default new Vuex.Store({
       if (response.status === 200) {
         context.commit("SET_USER", {
           email: credentials.email,
-          token: data.id,
           id: data.userId
         });
+        context.commit("ADD_AUTH", data.id);
         return { hasError: false, error: null };
       }
-      return { hasError: true, ...data }; // data wraps error payload
+      return { hasError: true, error: data.error }; // data wraps error payload
     },
     /**
      * Sign up the user with his credentials
@@ -60,7 +96,7 @@ export default new Vuex.Store({
      * @return {ApiErrorResponse}
      */
     SIGNUP: async (context, credentials) => {
-      let response = await fetch(`${API.ENDPOINT}/users`, {
+      const response = await fetch(`${API.ENDPOINT}/users`, {
         method: "POST",
         headers: API.HEADERS,
         body: JSON.stringify({
@@ -72,7 +108,44 @@ export default new Vuex.Store({
       if (response.status === 200) {
         return await context.dispatch("SIGNIN", credentials);
       }
-      return { hasError: true, ...data }; // data wraps error payload
+      return { hasError: true, error: data.error };
+    },
+    /**
+     * Fetch resource at a given url and call the specified
+     * mutation
+     * @param  {ActionContext} context Vuex action context
+     * @param  {Object} parameters Wrap url, mutation to call
+     *                            and optionally get parameters
+     * @return {ApiErrorResponse}
+     */
+    FETCH: async (context, parameters) => {
+      const response = await fetch(`${API.ENDPOINT}/${parameters.url}`, {
+        method: "GET",
+        headers: context.state.config.headers
+      });
+      const data = await response.json();
+      if (response.status === 200) {
+        context.commit(parameters.mutation, data);
+        return { hasError: false, error: null };
+      }
+      return { hasError: true, error: data.error };
+    },
+
+    /**
+     * The following actions call FETCH for specific resources
+     */
+    FETCH_CATEGORIES: async context => {
+      return await context.dispatch("FETCH", {
+        url: "categories",
+        mutation: "SET_CATEGORIES"
+      });
+    },
+
+    FETCH_SHOPS: async context => {
+      return await context.dispatch("FETCH", {
+        url: "shops",
+        mutation: "SET_SHOPS"
+      });
     }
   },
   modules: {}
