8ef6a30764765d533d1eb92999cd8b92b3b4188e
bigari <bigari7@gmail.com>
2019-11-23 23:14:10 +0100

    Add app bar with navigation

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="home">
-    <div>Home</div>
-    <router-view></router-view>
+    <v-app>
+      <v-app-bar app elevation="0">
+        <v-spacer></v-spacer>
+        <v-btn text to="/" class="no-deco" color="blue">Nearby Shops</v-btn>
+        <v-btn text to="/preferences" class="no-deco" color="blue"
+          >My preferred Shops</v-btn
+        >
+      </v-app-bar>
+      <v-content>
+        <v-container fluid>
+          <router-view></router-view>
+        </v-container>
+      </v-content>
+    </v-app>
   </div>
 </template>
 
