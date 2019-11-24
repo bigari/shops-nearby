99332a261b17d8d0ecd0def936d0a50ea8d18c6d
bigari <bigari7@gmail.com>
2019-11-24 01:54:26 +0100

    Create Vertically centered loading progress component

---
+++ frontend/src/components/Loading.vue
@@ -0,0 +1,28 @@
+<template>
+  <vertical-center>
+    <v-row>
+      <div class="mx-auto text-center">
+        <v-progress-circular
+          indeterminate
+          color="green"
+          :size="48"
+          class="mb-4"
+        />
+        <p class="mt-4 caption">{{ caption }}</p>
+      </div>
+    </v-row>
+  </vertical-center>
+</template>
+<script>
+import VerticalCenter from "@/components/VerticalCenter";
+
+export default {
+  name: "loading",
+  props: {
+    caption: String
+  },
+  components: {
+    VerticalCenter
+  }
+};
+</script>
