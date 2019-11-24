5a36617372776d1b6419d3504d108349b97e006f
bigari <bigari7@gmail.com>
2019-11-24 12:31:07 +0100

    Add link to google map with shop location

--- frontend/src/components/shop/ShopCard.vue
+++ frontend/src/components/shop/ShopCard.vue
@@ -6,15 +6,17 @@
         <v-spacer />
         <v-icon>{{ icon }}</v-icon>
       </v-card-title>
-      <v-img
-        class="white--text align-end font-weigth-bold title"
-        height="180px"
-        :src="shop.thumbnailUrl"
-      >
-        <v-overlay absolute v-if="shop.distance" opacity="0.6">
-          <p class="mx-2">{{ shop.distance.toFixed(2) }} Km</p>
-        </v-overlay>
-      </v-img>
+      <a :href="locationUrl" target="blank" class="no-deco">
+        <v-img
+          class="white--text align-end font-weigth-bold title"
+          height="180px"
+          :src="shop.thumbnailUrl"
+        >
+          <v-overlay absolute v-if="shop.distance" opacity="0.6">
+            <p class="mx-2">{{ shop.distance.toFixed(2) }} Km</p>
+          </v-overlay>
+        </v-img>
+      </a>
       <v-card-subtitle v-if="shop.distance"></v-card-subtitle>
       <v-card-actions>
         <slot />
@@ -41,6 +43,9 @@ export default {
   computed: {
     icon() {
       return this.faIcons[this.shop.category] || "fa-store";
+    },
+    locationUrl() {
+      return `https://www.google.com/maps/search/?api=1&query=${this.shop.location.lat},${this.shop.location.lng}`;
     }
   }
 };
