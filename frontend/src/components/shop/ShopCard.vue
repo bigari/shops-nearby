53fb9af63a96dc04ace8db057815fc191ac95e9c
bigari <bigari7@gmail.com>
2019-11-24 01:47:14 +0100

    Create shoping card

---
+++ frontend/src/components/shop/ShopCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-card outlined max-width="300">
+      <v-card-title>
+        <span>{{shop.name}}</span>
+        <v-spacer/>
+        <v-icon>{{ icon }}</v-icon>
+      </v-card-title>
+      <v-img class="white--text align-start" height="180px" :src="shop.thumbnailUrl"></v-img>
+      <v-card-actions>
+        <slot/>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+<script>
+export default {
+  name: "shop-card",
+  props: {
+    shop: Object
+  },
+  data() {
+    return {
+      faIcons: {
+        food: "fa-carrot",
+        clothing: "fa-tshirt",
+        toys: "fa-gamepad",
+        cosmetics: "fa-paint-brush"
+      }
+    };
+  },
+  computed: {
+    icon() {
+      return this.faIcons[this.shop.category] || "fa-store";
+    }
+  }
+};
+</script>
